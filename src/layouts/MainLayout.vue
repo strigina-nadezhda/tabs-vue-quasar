67008b0dad7c2e2c92b029f0e3c354e3f9df904a
strigina-nadezhda <126505183+strigina-nadezhda@users.noreply.github.com>
2024-10-15 15:19:50 +0300

    Add files via upload

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,78 +1,96 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header bordered class="bg-grey-1 text-primary q-mt-sm">
+    <q-header elevated>
       <q-toolbar>
-        <q-avatar rounded size="40px" class="q-pl-xs">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRz17l-3EsOtm8-Znyuj0otU2BLDz0OnY4MEvI3ojbUCJO-e9ANZ-8RRJGcYfuKIEXzoSk&usqp=CAU"
-          />
-        </q-avatar>
-        <q-space />
-        <q-btn flat round dense icon="menu" @click="showNotif" />
+        <q-tabs inline-label>
+          <q-route-tab name="requests" label="Заявки" to="/request" />
+          <q-route-tab name="accounts" label="Счета" to="/account" />
+          <q-route-tab
+            v-for="tab in reqStore.tabs"
+            :key="tab.id"
+            :name="tab.name"
+            :label="tab.label"
+            :to="`/request/${tab.id}`"
+          >
+            <q-btn
+              flat
+              dense
+              round
+              icon="close"
+              class="q-ml-xs"
+              @click.prevent="closeTab(tab.id)"
+            />
+          </q-route-tab>
+        </q-tabs>
       </q-toolbar>
-
-      <q-tabs dense indicator-color="primary" narrow-indicator inline-label>
-        <q-route-tab
-          v-for="tab in tabs"
-          :key="tab.name"
-          :to="tab.route"
-          :label="tab.label"
-        >
-          <q-icon
-            name="close"
-            size="1.2rem"
-            class="tab-icon"
-            v-if="!tab.main"
-            @click.prevent="closeTab(tab)"
-          />
-        </q-route-tab>
-      </q-tabs>
     </q-header>
 
-    <DialogForm />
-    <q-page-container animated>
-      <router-view :key="$route.fullPath" @close="closeTab" />
+    <q-page-container>
+      <router-view :key="route.params.id" />
     </q-page-container>
   </q-layout>
 </template>
 
-<script>
-import { defineComponent, ref } from "vue";
-import { mapState, mapActions } from "vuex";
-import DialogForm from "src/components/DialogForm.vue";
-import { useQuasar } from "quasar";
+<script setup>
+import { useRequestStore } from "src/stores/requests";
+import { useRouter, useRoute } from "vue-router";
+import { watch } from "vue";
+import { onMounted } from "vue";
 
-export default defineComponent({
+import { useAccountStore } from "src/stores/accounts";
+
+const accStore = useAccountStore();
+
+const reqStore = useRequestStore();
+
+defineOptions({
   name: "MainLayout",
+});
 
-  components: { DialogForm },
+// Используем useRoute для получения текущего маршрута
+const route = useRoute();
+const router = useRouter();
 
-  setup() {
-    const $q = useQuasar();
-    return {
-      activeTab: ref("accounts"),
-      showNotif() {
-        $q.notify({
-          timeout: 2500,
-          progress: true,
-          message: "Мне жаль, но тут пока ничего нет.",
-          color: "primary",
-          position: "top-right",
+// Отслеживаем изменения в текущем маршруте
+watch(
+  () => route.name,
+  (newName) => {
+    if (newName === "ReqDetails") {
+      const id = route.params.id;
+      const currentRow = reqStore.rows.find((row) => row.id === id);
+
+      // Проверяем, если вкладка уже существует, не добавляем её
+      if (!reqStore.tabIsOpen(id)) {
+        reqStore.addTab({
+          id: id,
+          label: `Заявка №${currentRow.num}`,
+          name: id,
         });
-      },
-    };
+      }
+      // else {
+      //   router.push(`/request/${id}`).catch((err) => {
+      //     console.error("Ошибка навигации:", err);
+      //   });
+      // }
+    }
   },
+  { immediate: true }
+);
 
-  computed: {
-    ...mapState({
-      tabs: (state) => state.allTabs.tabs,
-    }),
-  },
+// Закрытие таба
+const closeTab = (id) => {
+  reqStore.removeTab(id); // Удаляем вкладку из store
 
-  methods: {
-    ...mapActions({
-      closeTab: "allTabs/closeTab",
-    }),
-  },
+  if (route.path !== "/request") {
+    router.push("/request").catch((err) => {
+      console.error("Ошибка навигации:", err);
+    });
+  }
+};
+
+onMounted(() => {
+  if (accStore.rows.length <= 0) {
+    accStore.fetchAccountData();
+  }
 });
 </script>
